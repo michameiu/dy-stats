f182973dc7f8d438e80d688cd5642cc6a11df011
Mwangi Micha <michameiu@gmail.com>
2022-10-19 19:53:42 +0300

    Cleanup

--- projects/dy-stat-tables/src/public-api.ts
+++ projects/dy-stat-tables/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of dy-stat-tables
  */
 
-export * from './lib/dy-stat-tables.service';
+
 export * from './lib/dy-stat-tables.component';
 export * from './lib/dy-stat-tables.module';
 export * from './lib/dy-stat-tables.routing'
